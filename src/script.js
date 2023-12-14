44a7e2535e6267447a10a6afceebf168b3f280d3
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2023-12-14 02:12:46 +0100

    fixed import paths

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as THREE from './three'
+import { OrbitControls } from './three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
-import {DRACOLoader} from 'three/examples/jsm/loaders/DRACOLoader.js'
+import {GLTFLoader} from './three/examples/jsm/loaders/GLTFLoader.js'/* 
+import {DRACOLoader} from 'three/examples/jsm/loaders/DRACOLoader.js' */
 
 // IMPORT FILES
 
