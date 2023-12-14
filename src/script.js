143d0bb597afdc6d30f536c08657a0b7e5fb0774
CarlaCGDM <nccardillo@iesrafaelalberti.es>
2023-12-14 14:59:01 +0100

    gdfgdgd

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
-import * as THREE from '/../node_modules/three'
-import { OrbitControls } from '/../node_modules/three/examples/jsm/controls/OrbitControls.js'
-import GUI from '/../node_modules/lil-gui'
-import {GLTFLoader} from '/../node_modules/three/examples/jsm/loaders/GLTFLoader.js'
+import * as THREE from '/three'
+import { OrbitControls } from '/three/examples/jsm/controls/OrbitControls.js'
+import GUI from '/lil-gui'
+import {GLTFLoader} from '/three/examples/jsm/loaders/GLTFLoader.js'
 
 // IMPORT FILES
 
